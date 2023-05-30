fcf5e2c968206e4ad16c81ec8f38f21bf7f35285
Tamás <t.gabor80312@gmail.com>
2023-05-30 16:04:25 +0200

    updated nav links, socials, copyright

---
+++ src/datas/footer/navigation.js
@@ -0,0 +1,82 @@
+export const navigation = {
+	col1: {
+		label: "Sitemap",
+		links: [
+			{
+				text: "Home",
+				url: "/",
+				type: "internal",
+			},
+			{
+				text: "Development",
+				url: "/developer-portal/",
+				type: "internal",
+			},
+			{
+				text: "Team",
+				url: "/team/",
+				type: "internal",
+			},
+			{
+				text: "Resources",
+				url: "/resources/",
+				type: "internal",
+			},
+			{
+				text: "Community",
+				url: "/community/",
+				type: "external",
+			},
+			{
+				text: "Careers",
+				url: "/careers/",
+				type: "internal",
+			},
+            {
+				text: "FAQ",
+				url: "/faq/",
+				type: "internal",
+			},
+		],
+	},
+	col2: {
+		label: "Developer",
+		links: [
+			{
+				text: "View our GitHub repos",
+				url: "/",
+				type: "internal",
+			},
+			{
+				text: "Read the protocol specifications",
+				url: "/",
+				type: "internal",
+			},
+			{
+				text: "Read the original research paper",
+				url: "/",
+				type: "internal",
+			},
+		],
+	},
+	col3: {
+		label: "Community",
+		links: [
+			{
+				text: "Join our Discord community",
+				url: "https://discord.com/invite/YsnTPcSfWQ",
+				type: "external",
+			},
+			{
+				text: "Join our Telegram group",
+				url: "https://t.me/CelestiaCommunity",
+				type: "internal",
+			},
+			{
+				text: "Follow us on Twitter",
+				url: "https://twitter.com/CelestiaOrg",
+				type: "external",
+			},
+		],
+	},
+};
