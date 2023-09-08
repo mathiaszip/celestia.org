fc1a71037192253ca0ebafe8b028e7fac205c300
Tamás <t.gabor80312@gmail.com>
2023-09-08 16:58:41 +0200

    fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -239,46 +239,6 @@ class Footer extends React.Component {
 						</div>
 					</div>
 
-                    <FooterBox footerBoxes={this.props.FooterBoxes}/>
-
-                    <div className={'row py-5 mt-5'}>
-                        <div className={'col col-12 col-lg-4 pe-5'}>
-                            <div className={'title'}>Subscribe to our Newsletter</div>
-
-                            <form onSubmit={(e) => this._handleSubmit(e)} className={'needs-validation'}>
-                                <input type="email" id={'email'} className={'form-control'} onChange={(e) => this.change(e)} required/>
-                                <button type={'submit'} className={'button button-simple mt-3'}>Subscribe</button>
-                            </form>
-
-                        </div>
-                        <div className={'col col-12 col-lg-8 ps-3 pt-5 pt-lg-0 ps-lg-5'}>
-                            <div className={'row'}>
-                                <div className={'col col-12 col-sm-6 col-lg-6'}>
-                                    <div className={'col-title'}>Sitemap</div>
-                                    <nav>
-                                        <ul>
-                                            {navigation.map(link => (
-                                                <li key={`${link.url}`}>
-                                                    {link.type === 'external' ? <a href={`${link.url}`} target={'_blank'} rel={'noreferrer'}>{link.text}</a> : <Link to={`${link.url}`}>{link.text}</Link>}
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </nav>
-                                </div>
-                                <div className={'col col-12 col-sm-6 col-lg-6 align-right pt-5 pt-sm-0'}>
-                                    <div className={'col-title text-right'}>Follow us</div>
-                                    <div className={'socials'}>
-                                        <Discord url={'https://discord.com/invite/YsnTPcSfWQ'}/>
-                                        <Github url={'https://github.com/celestiaorg'}/>
-                                        <Email url={'https://t.me/CelestiaCommunity'}/>
-                                        <Twitter url={'https://twitter.com/CelestiaOrg'}/>
-                                        <Youtube url={'https://www.youtube.com/channel/UCLlvAEzXBFZ-P3zS6BF2Bjg'}/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                     <div className={'row justify-content-center'}>
                         <div className={'col col-12 col-sm-6'}>
                             <p>This website is maintained by Celestia Labs.</p>
